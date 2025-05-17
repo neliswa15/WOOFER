18fb277eadedf1b14059fcde0290c41b757bb7be
ZaydenSuresh <2610978@students.wits.ac.za>
2025-05-17 16:38:20 +0200

    Update EmailVerificationActivity.java

--- app/src/main/java/com/example/woofer/EmailVerificationActivity.java
+++ app/src/main/java/com/example/woofer/EmailVerificationActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class EmailVerification extends AppCompatActivity {
+public class EmailVerificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
